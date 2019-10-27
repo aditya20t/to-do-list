673b79fae57565553092c57d628c0e54ebebd4d7
Aditya Tomar <tomaraditya619@gmail.com>
2019-10-27 07:24:22 +0530

    made changes

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.post("/", function(req,res){
 	res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Server has started on port 3000");
 });
